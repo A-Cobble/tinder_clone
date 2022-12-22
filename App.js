ade9dc764c059d343c05b5abd48e14a4056603fd
A-Cobble <alex.cobble42@gmail.com>
2022-12-22 14:55:30 -0800

    removed expo-google-app-auth due to it not working and being deprecated, added expo-auth-session and expo-random for authentication. Added a scheme to app.json so that the app wouldn't crash. Commented out the AuthProvider/custom hook due to an error saying invalid hook usage. Added google authentication to the loginScreen. Information now needs to be sent to all other applicable pages.

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import tw from "twrnc"
-import { AuthProvider } from './hooks/useAuth';
+// import { AuthProvider } from './hooks/useAuth';
 import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
       {/* HOC - Higher Order Componenet */}
-      <AuthProvider>
+      {/* <AuthProvider> */}
         {/* Passes down the auth stuff to children... */}
         <StackNavigator />
-      </AuthProvider>
+      {/* </AuthProvider> */}
     </NavigationContainer>
   );
 }
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,19 +1,33 @@
 import { View, Text } from 'react-native'
 import React, { createContext, useContext } from 'react'
-import * as AuthSession from "expo-auth-session"
+import * as Google from "expo-auth-session/providers/google"
 
-const AuthContext = createContext({})
+const AuthContext = createContext({});
+
+const config = {
+  androidClientId: "347959046486-c0vbabnvmd0fvqd0ctkuu6p1moeeiaup.apps.googleusercontent.com",
+  iosClientId: '347959046486-n5oteg6ig9eea9esfi1ugou0lg4pn6pl.apps.googleusercontent.com',
+  // androidClientId: process.env.ANDROID_CLIENT_ID,
+  // iosClientId: process.env.IOS_CLIENT_ID,
+  // scopes: ["profile", "email"],
+  // permissions: ["public_profile", "email", "gender", "location"],
+}
 
 export const AuthProvider = ({ children }) => {
 
-  const signInWithGoogle = async() => {
-    // await AuthSession.
+  const signInWithGoogle = () => {
+    Google.useAuthRequest(config).then((logInResult) => {
+      if(logInResult?.type === "success"){
+        //login...
+      }
+    });
   }
 
   return (
     <AuthContext.Provider 
       value={{
         user: null,
+        signInWithGoogle
       }}
     >
       {children}
