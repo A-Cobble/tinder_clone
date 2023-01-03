a78941466240514b758bd756f1bc955e937054f6
A-Cobble <alex.cobble42@gmail.com>
2023-01-03 13:44:35 -0800

    added dummy data and styling to create cards. Limited swipping of cards to left and right. Added shadows to the cards to make them stand out more by adding a manual stylesheet.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,39 @@
-import { View, Text, Button, SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import { View, Text, Button, SafeAreaView, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import useAuth from '../hooks/useAuth';
 import tw from "twrnc"
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
+
+
+const DUMMY_DATA = [
+  {
+    firstName: "Alexander",
+    lastName: "Cobble",
+    job: "Software Developer",
+    // photoURL: "https://avatars.githubusercontent.com/u/24712956?v=4",
+    photoURL: "https://images.unsplash.com/photo-1664737061963-862d6a174a3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    age: 32,
+    id: 123,
+  },
+  {
+    firstName: "Name1",
+    lastName: "LastName1",
+    job: "Software Developer",
+    photoURL: "https://images.unsplash.com/photo-1670862386920-e5321e7b47c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
+    age: 40,
+    id: 456,
+  },
+  {
+    firstName: "Blah",
+    lastName: "BlahSquared",
+    job: "Software Developer",
+    photoURL: "https://images.unsplash.com/photo-1671085957836-60848fafaf75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
+    age: 21,
+    id: 789
+  }
+]
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -16,7 +46,7 @@ const HomeScreen = () => {
   // },[])
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw `flex-1`}>
 
       {/* Header */}
       <View /*style={tw`items-center relative`}*/ style={tw`flex-row items-center justify-between px-5`}>
@@ -31,8 +61,43 @@ const HomeScreen = () => {
           <Ionicons name="chatbubbles-sharp" size={30} color="#FF5864" />
         </TouchableOpacity>
       </View>
+      {/* End of Header */}
 
-      {/* End */}
+      {/* Cards */}
+      <View style={tw`flex-1 -mt-6`}>
+        <Swiper 
+          containerStyle={{ backgroundColor: "transparent" }}
+          stackSize={5}
+          cardIndex={0}
+          animateCardOpacity
+          verticalSwipe={false}
+          cards={DUMMY_DATA} 
+          renderCard={(card) => (
+            <View key={card.id} style={tw `relative bg-white h-3/4 rounded-xl`}>
+              <Image style={tw `absolute top-0 h-full w-full rounded-xl`} source={{ uri: card.photoURL}}/>
+              
+              <View style={[tw `absolute bottom-0 flex-row justify-between items-between bg-white w-full h-20 px-6 py-2 rounded-b-xl`, styles.cardShadow]}>
+                <View>
+                  <Text style={tw `text-xl font-bold`}>
+                    {card.firstName} {card.lastName}
+                  </Text>
+                  <Text>
+                    {card.job}
+                  </Text>
+                </View>
+                <Text style={tw `text-2xl font-bold`}>
+                  {card.age}
+                </Text>
+              </View>
+            </View>
+            )
+          }
+        />
+      </View>
+
+
+
+
       {/* <Text>{`Hello and welcome to the Tinder_Clone`}</Text>
       <Button title="Go to Chat Screen" onPress={() => navigation.navigate("Chat")}/>
 
@@ -41,4 +106,18 @@ const HomeScreen = () => {
   )
 }
 
-export default HomeScreen
+export default HomeScreen;
+
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  }
+})
